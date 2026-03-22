871c2d0bc5aad000f2b61aa31b2da8772aaf794f
Sachila2003 <sachilageeth1@gmail.com>
2026-03-22 21:34:22 +0530

    authcontroller done

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    public function register(Request $request) {
+        $data = $request->validate([
+            'name' => 'required|string|max:225',
+            'email' => 'required|string|email|unique:users',
+            'password' => 'required|string|min:8|confirmed',
+            'department_id' => 'required|exists:departments,id'
+        ]);
+
+        $user = User::create([
+            'name' =>$data['name'],
+            'email' => $data['email'],
+            'password' => Hash::make($data['password']),
+            'department_id' => $data['department_id'],
+            'role' => 'admin'
+        ]);
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()-> json([
+            'message' => "User Registered successfully",
+            'user' => $user,
+            'token' => $token
+        ], 201);
+        
+    
+    }
+
+    //user login logic
+    public function login(Request $request) {
+        $data = $request-> validate([
+            'email' => 'required|email',
+            'password' => 'required'
+        ]);
+
+        $user = User::where('email', $data['email'])->first();
+
+        if (!$user || !Hash::compare($data['password'], $user->password)) {
+            return response()->json([
+                'message' => 'Invalid credentials'
+            ], 401);
+        }
+
+        $token = $user->createToken('auth_token') ->plainTextToken;
+
+        return response()->json([
+            'message' => 'User logged in successfully',
+            'user' => $user,
+            'token' => $token
+        ], 200);
+    }
+}
